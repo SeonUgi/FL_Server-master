7587b76d33adca0ec1dd525b20b1019c9025a49d
Woogie <king950411@gmail.com>
2021-11-29 04:29:29 +0900

    debug ecg topk avg

--- app/Federated.py
+++ app/Federated.py
@@ -138,7 +138,7 @@ class FederatedServer:
             cls.local_weights = []  # global weight average 이후 다음 라운드를 위해 이전의 local weight 리스트 초기화
 
         elif algorithm == 'topk':
-            top = 2
+            top = 1
             print(cls.local_accs)
             temp = []
             for i in cls.local_accs:
